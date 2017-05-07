bdf2c168d3b6335fa8341e7c22e50cf0e084d55b
tbalint19 <toth910719balint@gmail.com>
2017-05-07 20:11:49 +0200

    folders created

---
+++ apps/webpack.config.js
@@ -0,0 +1,38 @@
+var webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+
+  entry: {
+     home: './home/main.jsx',
+     list: './list/main.jsx',
+     profile: './profile/main.jsx'
+  },
+
+  output: {
+      path: '../todo_app/_apps/',
+      filename: "[name].js",
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      }
+    ],
+  },
+
+  resolve: {
+    modulesDirectories: ['node_modules', 'common_components/css', 'common_components/js'],
+    extensions: ['', '.js', '.jsx', '.css']
+  },
+}
---
+++ todo_app/app_list/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AppListConfig(AppConfig):
+    name = 'app_list'

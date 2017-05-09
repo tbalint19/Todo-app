d211e9b3479ef634c16d75af3b0dd403dac7e582
tbalint19 <toth910719balint@gmail.com>
2017-05-09 11:31:13 +0200

    profile and todo models are created

--- todo_app/app_list/models.py
+++ todo_app/app_list/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=30)
+    text = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)

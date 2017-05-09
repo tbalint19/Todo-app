9ce4a8451dc4b01c8a2b13e80ee6e0b6309abc79
tbalint19 <toth910719balint@gmail.com>
2017-05-09 13:57:27 +0200

    list views created

--- todo_app/app_list/models.py
+++ todo_app/app_list/models.py
@@ -1,9 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class TodoManager(models.Manager):
+
+    def get_todos(self, user):
+        return [dict(todo) for todo in self.filter(user=user)]
+
 class Todo(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
     title = models.CharField(max_length=30)
     text = models.CharField(max_length=150)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __iter__(self):
+        for field in ['id', 'title', 'text', 'created']:
+            yield field, getattr(self, field)
+
+    def update(self, title, text):
+        self.title = title
+        self.text = text
+        self.save()
--- todo_app/app_list/views.py
+++ todo_app/app_list/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+import json
 
 def get_list(request):
     return render(request, 'list.html')
 
 def create_todo(request):
-    return JsonResponse({})
+    is_authenticated = request.user is not None
+    if is_authenticated:
+        todo_data = json.loads(request.body.decode("utf-8"))
+        todo = Todo(user=request.user, title=todo_data["title"], text=todo_data["text"])
+        todo.save()
+    return JsonResponse({"created": True})
 
 def get_todos(request):
-    return JsonResponse({})
+    is_authenticated = request.user is not None
+    if is_authenticated:
+        todos = Todo.get_todos(request.user)
+    return JsonResponse({'data': todos})
 
 def update_todos(request):
-    return JsonResponse({})
+    is_authenticated = request.user is not None
+    if is_authenticated:
+        all_data = json.loads(request.body.decode("utf-8"))
+        for todo_data in all_data:
+            todo = Todo.objects.get(id=todo_data["id"])
+            todo.update(title=todo_data["title"], text=todo_data["text"])
+    return JsonResponse({"updated": is_authenticated})
 
 def delete_todo(request):
-    return JsonResponse({})
+    is_authenticated = request.user is not None
+    if is_authenticated:
+        todo_id = json.loads(request.body.decode("utf-8"))["id"]
+        Todo.objects.get(id=todo_id).delete()
+    return JsonResponse({"deleted": is_authenticated})
